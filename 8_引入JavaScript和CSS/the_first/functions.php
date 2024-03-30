2e03850618df0789905aa385024af81f835224bc
杨江河 <longwenjunj@163.com>
2024-03-30 16:00:45 +0800

    8_引入JavaScript和CSS

---
+++ 8_引入JavaScript和CSS/the_first/functions.php
@@ -0,0 +1,62 @@
+<?php if (!defined('ABSPATH')) {
+    die;
+} // Cannot access directly.
+?>
+
+<?php
+
+/**
+ * 追格WordPress主题开发教程
+ */
+
+require_once TEMPLATEPATH . '/inc/codestar-framework/codestar-framework.php';
+require_once TEMPLATEPATH . '/inc/admin-options.php';
+
+
+function the_first_scripts()
+{
+    $url = get_template_directory_uri();
+    wp_register_script('lib-script', $url . '/js/lib/lb.js', [], '0.1');
+    wp_register_script('the-index-script', $url . '/js/index.js', ['lib-script'], '0.1', true);
+
+    if (is_home()) {
+        wp_enqueue_script('lib-script');
+        wp_enqueue_script('the-index-script');
+    }
+}
+add_action('wp_enqueue_scripts', 'the_first_scripts');
+
+
+/**
+ * 读取设置项的值
+ */
+$the_first_options = null;
+function the_first_option($key, $default = '')
+{
+    global $the_first_options;
+    if (!$the_first_options) {
+        $the_first_options = get_option('zhuige_the_first');
+    }
+
+    if (isset($the_first_options[$key])) {
+        return $the_first_options[$key];
+    }
+
+    return $default;
+}
+
+/**
+ * 显示网站LOGO
+ */
+function the_first_site_logo()
+{
+    $logo = the_first_option('site_logo');
+    if ($logo && $logo['url']) {
+        $logo_src = $logo['url'];
+    } else {
+        $logo_src = get_stylesheet_directory_uri() . '/images/xlogo.png';
+    }
+
+    echo '<img alt="picture loss" src="' . $logo_src . '">';
+}
+			
---
+++ 8_引入JavaScript和CSS/the_first/header.php
@@ -0,0 +1,55 @@
+<?php if (!defined('ABSPATH')) {
+    die;
+} // Cannot access directly.
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	<title>The First 主题</title>
+	<?php wp_head(); ?>
+	<!-- <link rel="stylesheet" href="style.css"> -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+
+<body>
+	<header id="top-nav-wraper">
+		<!--主导航-->
+		<nav class="container">
+			<div class="menu-icon">
+				<span class="fas">
+					<img alt="picture loss" src="<?php echo get_stylesheet_directory_uri() . '/images/fa-menu.png' ?>">
+				</span>
+			</div>
+			<a class="logo" href="<?php echo home_url(); ?>"><?php the_first_site_logo() ?></a>
+			<div class="nav-box">
+				<ul class="nav-items">
+					<li><a href="javascript:void(0)" aria-current="page">首页</a></li>
+					<li><a href="javascript:void(0)">主题下载</a></li>
+					<li><a href="javascript:void(0)">小程序下载</a></li>
+					<li><a href="javascript:void(0)">社区</a></li>
+					<li><a href="javascript:void(0)">网址导航</a></li>
+					<li><a href="javascript:void(0)">标签云</a></li>
+					<li><a href="javascript:void(0)">关于</a></li>
+				</ul>
+			</div>
+			<div class="search-icon">
+				<span class="fas">
+					<img alt="picture loss" src="<?php echo get_stylesheet_directory_uri() . '/images/fa-search.png' ?>">
+				</span>
+			</div>
+			<div class="cancel-icon">
+				<span class="fas">
+					<img alt="picture loss" src="<?php echo get_stylesheet_directory_uri() . '/images/fa-close.png' ?>">
+				</span>
+			</div>
+			<form method="get" action="/">
+				<input type="search" class="search-data" placeholder="输入搜索内容" value="" name="s" id="s" required="" style="color:black;">
+				<button type="submit">搜索</button>
+			</form>
+		</nav>
+	</header>
