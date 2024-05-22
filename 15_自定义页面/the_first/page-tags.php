97dabb27c3a9239e5d84eda109730b44c31b0a86
杨江河 <longwenjunj@163.com>
2024-05-22 17:14:36 +0800

    15_自定义页面

---
+++ 15_自定义页面/the_first/page-tags.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Template Name: 追格-标签云
+*/
+?>
+
+<?php if (!defined('ABSPATH')) {
+    die;
+} // Cannot access directly.
+?>
+
+<!-- 页头 -->
+<?php get_header(); ?>
+
+<div class="main-body mt-20">
+	<div class="container">
+		<div class="row d-flex flex-wrap">
+			<!--主内容区-->
+			<article class="column xs-12 sm-12 md-8 mb-10-xs mb-0-md">
+
+				<div class="base-list search-list mb-20">
+					<div class="base-list-nav" itemscope=""><a itemprop="breadcrumb" href="/">首页</a> <em> &gt; </em>
+						<span class="current">标签云</span>
+					</div>
+					<div class="content-wrap">
+						<div class="tag-list">
+							<a title="46个话题" href="javascript:void(0)">酱茄 (46)</a>
+							<a title="33个话题" href="javascript:void(0)">小程序 (33)</a>
+							<a title="23个话题" href="javascript:void(0)">酱茄小程序 (23)</a>
+							<a title="14个话题" href="javascript:void(0)">酱茄pro (14)</a>
+							<a title="13个话题" href="javascript:void(0)">WordPress (13)</a>
+							<a title="11个话题" href="javascript:void(0)">追格 (11)</a>
+							<a title="9个话题" href="javascript:void(0)">酱茄Free (9)</a>
+							<a title="9个话题" href="javascript:void(0)">WordPress小程序 (9)</a>
+							<a title="5个话题" href="javascript:void(0)">酱茄cms (5)</a>
+							<a title="5个话题" href="javascript:void(0)">WordPress主题 (5)</a>
+							<a title="4个话题" href="javascript:void(0)">酱茄主题 (4)</a>
+							<a title="4个话题" href="javascript:void(0)">追格资讯小程序 (4)</a>
+							<a title="4个话题" href="javascript:void(0)">酱茄小程序pro (4)</a>
+							<a title="4个话题" href="javascript:void(0)">酱茄Free小程序 (4)</a>
+							<a title="4个话题" href="javascript:void(0)">追格小程序 (4)</a>
+							<a title="4个话题" href="javascript:void(0)">资讯小程序 (4)</a>
+							<a title="3个话题" href="javascript:void(0)">The First 主题 (3)</a>
+							<a title="3个话题" href="javascript:void(0)">WordPress插件 (3)</a>
+							<a title="2个话题" href="javascript:void(0)">小程序源码 (2)</a>
+							<a title="2个话题" href="javascript:void(0)">酱茄plus (2)</a>
+							<a title="2个话题" href="javascript:void(0)">更新日志 (2)</a>
+							<a title="2个话题" href="javascript:void(0)">酱茄pro小程序 (2)</a>
+							<a title="1个话题" href="javascript:void(0)">商城小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格企业官网小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">mp-html (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格推广 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格分销 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格圈子小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">微信流量主小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">代码高亮 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格企业官网主题 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格商城小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">企业官网主题 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">追格马甲生成器 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">酱茄开源版 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">分享朋友圈 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">人人咖啡师 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">WordPress云存储插件 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">WordPress博客小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">微信小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">WordPress小程序插件 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">酱茄企业官网 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">酱茄OW (1)</a>
+							<a title="1个话题" href="javascript:void(0)">官网小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">企业官网小程序 (1)</a>
+							<a title="1个话题" href="javascript:void(0)">酱茄小程序开源版 (1)</a>
+						</div>
+					</div>
+				</div>
+
+			</article>
+
+			<!--侧边栏-->
+			<?php get_sidebar(); ?>
+
+		</div>
+	</div>
+</div>
+
+<!-- 页脚 -->
+<?php get_footer(); ?>
